8fc33802ae27f45e5a8ec7c7e37ace8ce7a6409f
Kirsten Bachert <kb@buildmidwestern.com>
2024-05-28 17:43:23 -0600

    fix: added mobile material ui footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,13 @@ import NovaPerson2 from "../../public/NovaPerson2.png";
 import HeartIcon from "../../public/heartIcon.svg";
 import HeroGen1 from "../../public/heroGen1.jpg";
 import Purple8 from "../../public/purple8.jpg";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 export default function Home() {
   const featuredCards = [
@@ -132,6 +139,13 @@ export default function Home() {
         backgroundColor="bg-black"
         additionalSection={true}
       />
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation>
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Box>
     </div>
   );
 }
