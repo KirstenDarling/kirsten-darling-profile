6dd4caf66911652d53db31eff0165b1e04231534
Kirsten Bachert <kb@buildmidwestern.com>
2024-06-04 11:09:04 -0600

    feat: added in Google Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import GoogleAnalytics from "../app/components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      <GoogleAnalytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
