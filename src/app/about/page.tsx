60b4d4e28dce2a00a20efcce029736ffcf945cdf
Kirsten Bachert <kb@buildmidwestern.com>
2024-07-30 17:32:18 -0600

    fix: tech debt deleted unused imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,31 +1,10 @@
 "use client";
 
-import { useState } from "react";
-
-import CombinedNavBar from "../components/CombinedNavBar";
-import { FaAddressCard, FaDatabase } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
-import { FaHouseLaptop } from "react-icons/fa6";
-import { FaLaptopCode } from "react-icons/fa";
-import NovaPerson2 from "../../public/NovaPerson2.png";
-import HeartIcon from "../../public/heartIcon.svg";
-import HeroGen1 from "../../public/heroGen1.jpg";
-import Purple8 from "../../public/purple8.jpg";
 import * as React from "react";
-import Image from "next/image";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import { GitHub, LinkedIn } from "@mui/icons-material";
-import BuildingSVG from "../../../public/building.svg";
 import Timeline from "../components/Timeline";
 import ProfileImage from "../../../public/profile.jpg";
 import HelpingImage from "../../../public/kirstenhelpingatmwi.png";
 import WorkplaceImage from "../../../public/MWI.jpg";
-import ProfessionalJourneyImage from "../../../public/professionalJourneyImage.png";
 
 export default function Home() {
   // move below variables to Contenful
