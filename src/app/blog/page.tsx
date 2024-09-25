0e51d237fbb848b0d5a473fc46c7f56468d37768
Kirsten Bachert <kb@buildmidwestern.com>
2024-09-24 23:50:36 -0600

    [fix] initial rough draft of blog overview page layout

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { client } from "../../sanity/lib/client";
 import React from "react";
 import ComingSoonSection from "../components/ComingSoonSection";
+import CertificationCards from "../components/CertificationCards";
 
 const BlogIndex = () => {
   const [posts, setPosts] = useState<
@@ -35,6 +36,65 @@ const BlogIndex = () => {
           </li>
         ))}
       </ul> */}
+        <div className="blog-card-layout flex flex-row">
+          <div className="blog-card-layout-left flex flex-col pl-[5rem] pr-[2.5rem] my-[5rem]">
+            <div className="blog-card-headling-article flex flex-col">
+              <div className="headling-article-image">Image</div>
+              <div className="headling-article-title">Title</div>
+              <div className="headling-article-excerpt">
+                article exceprt blah blah blah article exceprt blah blah blah
+                article exceprt blah blah blah
+              </div>
+            </div>
+          </div>
+          <div className="blog-card-layout-right flex flex-col pl-[2.5rem] pr-[5rem] my-[5rem]">
+            <div className="blog-card-list-of-articles">
+              <div className="article-in-list flex flex-row">
+                <div className="headling-article-image">Image</div>
+                <div className="headling-article-grouping flex flex-col">
+                  <div className="headling-article-title">article title</div>
+                  <div className="headling-article-excerpt">
+                    article exceprt blah blah blah article exceprt blah blah
+                    blah article exceprt blah blah blah
+                  </div>
+                </div>
+              </div>
+              <div className="article-in-list flex flex-row">
+                <div className="headling-article-image">Image</div>
+                <div className="headling-article-grouping flex flex-col">
+                  <div className="headling-article-title">article title</div>
+                  <div className="headling-article-excerpt">
+                    article exceprt blah blah blah article exceprt blah blah
+                    blah article exceprt blah blah blah
+                  </div>
+                </div>
+              </div>
+              <div className="article-in-list flex flex-row">
+                <div className="headling-article-image">Image</div>
+                <div className="headling-article-grouping flex flex-col">
+                  <div className="headling-article-title">article title</div>
+                  <div className="headling-article-excerpt">
+                    article exceprt blah blah blah article exceprt blah blah
+                    blah article exceprt blah blah blah
+                  </div>
+                </div>
+              </div>
+              <div className="article-in-list flex flex-row">
+                <div className="headling-article-image">Image</div>
+                <div className="headling-article-grouping flex flex-col">
+                  <div className="headling-article-title">article title</div>
+                  <div className="headling-article-excerpt">
+                    article exceprt blah blah blah article exceprt blah blah
+                    blah article exceprt blah blah blah
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog-card-layout-right flex flex-col pl-[2.5rem] pr-[5rem]">
+          <CertificationCards cards={[]} />
+        </div>
       </div>
     </div>
   );
