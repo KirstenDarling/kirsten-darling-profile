ab73ac7de77930a8aa0790bdf91ad1f29999ad19
Kirsten Bachert <kb@buildmidwestern.com>
2024-09-30 19:00:06 -0600

    [fix] playing around with study room styles

--- src/app/study-portal/page.tsx
+++ src/app/study-portal/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import ComingSoonSection from "../components/ComingSoonSection";
 import StudyBuddy from "../components/StudyBuddyCircle";
@@ -27,9 +29,14 @@ export default function Home() {
     },
   ];
 
+  const [matchingVerb, setMatchingVerb] = React.useState("Play");
+
   const flashCardLink = {
-    name: "Flash Cards",
-    url: "https://www.example-style-guide.com",
+    name: `${matchingVerb} Matching Game`,
+    onClick: () => {
+      setIsShown(!isShown);
+      setMatchingVerb("Close");
+    },
   };
 
   const cheatSheetLink = {
@@ -42,14 +49,59 @@ export default function Home() {
     url: "https://www.example-style-guide.com",
   };
 
+  const [isShown, setIsShown] = React.useState(false);
+  const [iframePosition, setIframePosition] = React.useState({ x: 0, y: 0 });
+
+  const handleDragStart = (e: React.DragEvent<HTMLIFrameElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    e.dataTransfer.setData(
+      "text/plain",
+      JSON.stringify({
+        offsetX: e.clientX - rect.left,
+        offsetY: e.clientY - rect.top,
+      })
+    );
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const data = JSON.parse(e.dataTransfer.getData("text/plain"));
+    setIframePosition({
+      x: e.clientX - data.offsetX,
+      y: e.clientY - data.offsetY,
+    });
+  };
+
   return (
     <div className="w-full h-[100vh] bg-purple-500 flex-col justify-start items-center inline-flex">
-      <StudyBuddy resources={resources} />
-      <div className="flex flex-row">
-        <Pill link={flashCardLink} />
-        <Pill link={cheatSheetLink} />
-        <Pill link={roadmapLink} />
-      </div>
+      {/* <StudyBuddy resources={resources} />
+
+      <Pill link={flashCardLink} />
+      <Pill link={cheatSheetLink} />
+      <Pill link={roadmapLink} />
+      <div className="flex flex-row bg-red-200"> */}
+
+      <iframe
+        className={`${isShown ? `absolute ${window.innerWidth <= 768 ? "top-[8%]" : "top-[15%]"} z-[1]` : "hidden"}`}
+        src="https://quizlet.com/950214239/match/embed?i=1rmz5e&x=1jj1"
+        height="110%"
+        width={window.innerWidth <= 768 ? "100%" : "500"}
+        style={{ border: 0 }}
+        draggable="true"
+        onDragStart={handleDragStart}
+      ></iframe>
+
+      <Pill link={flashCardLink} />
+      <iframe
+        src="https://roadmap.sh/r/embed?id=66fb3c7ce52a9bf4762b6115"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 }
